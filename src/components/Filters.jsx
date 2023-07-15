1c6e39cf7e02da0f1ec1e67af09fc0d5fafc247c
antikevin <Kevin_3467@yahoo.com>
2023-07-15 20:54:30 -0300

    feat: criando component de filtragem

---
+++ src/components/Filters.jsx
@@ -0,0 +1,72 @@
+import { Box, Button, Stack } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+
+const Filters = ({dataInicio, 
+                  setDataInicio, 
+                  dataFim, 
+                  setDataFim, 
+                  nomeOperador, 
+                  setNomeOperador, 
+                  setButtonClickCount 
+                }) => {
+  return (
+    <Box
+    sx={{
+      width: 800,
+      height: 300,
+    }}
+  >
+    <Stack 
+      direction="row"
+      spacing={2}
+      justifyContent="center"
+      height={"70%"}
+      alignItems="center"
+    >
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker value={dataInicio} 
+          format="DD/MM/YYYY" 
+          label="Data de Inicio" 
+          onChange={(newValue) => setDataInicio(newValue)} 
+        />
+        <DatePicker 
+          value={dataFim} 
+          format="DD/MM/YYYY" 
+          label="Data de Fim" 
+          onChange={(newValue) => setDataFim(newValue)} 
+        />
+        </LocalizationProvider >
+        <TextField 
+          id="outlined-basic" 
+          label="Nome do Operador Transacionado" 
+          variant="outlined" 
+          value={nomeOperador} 
+          onChange={(event) => setNomeOperador(event.target.value)}
+        />
+    </Stack>
+    <Stack 
+      direction="row"
+      justifyContent="end"
+      height={"13%"}
+      marginRight={"2%"}
+    >
+      <Button 
+        label="Helper text example" 
+        variant="contained"
+        onClick={() => {
+          setButtonClickCount(prevCount => prevCount + 1);
+        }}
+        >
+        Filtrar
+      </Button>
+    </Stack>
+    
+  </Box>   
+  )
+}
+
+export default Filters
