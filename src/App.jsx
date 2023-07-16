cdfe463a9ff06f68f048ec5f6907258a13b21b0d
antikevin <Kevin_3467@yahoo.com>
2023-07-15 21:04:24 -0300

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from "react";
+
 import Table from "./components/Table";
 
 import Stack from '@mui/material/Stack';
 import axios from "axios";
+import Filters from "./components/Filters";
 
 
 function App() {
 
   const [apiData, setApiData] = useState([])
   const [dataLoading, setDataLoading] = useState(true)
-  const [nomeOperador, setNomeOperador] = useState(null)
-  const [dataInicio, setDataInicio] = useState('')
-  const [dataFim, setDataFim] = useState('')
+  const [nomeOperador, setNomeOperador] = useState('')
+  const [dataInicio, setDataInicio] = useState(undefined)
+  const [dataFim, setDataFim] = useState(undefined)
+  const [filtroClickCount, setFiltroClickCount] = useState(0);
 
   useEffect(() => {
+
+    
     const fetchData = async () => {
       try {
+        setDataLoading(true);
         const response = await axios.get('http://localhost:8080/api/transferencia',{
 			params: {
-				nomeOperador: nomeOperador,
-				dataInicio: dataInicio,
-				dataFim: dataFim,
+				nomeOperador: nomeOperador ? nomeOperador : null,
+				dataInicio: dataInicio ? dataInicio.format('YYYY-MM-DDTHH:mm:ssZ') : '',
+				dataFim: dataFim ? dataFim.format('YYYY-MM-DDTHH:mm:ssZ') : '',
 			}
 		});
         setApiData(response.data);
@@ -33,11 +39,28 @@ function App() {
 
     fetchData();
 
-  }, []);
+  }, [filtroClickCount]);
   
 
   return (
     <>
+	<Stack
+		direction="row"
+		justifyContent="center"
+		alignItems="center"
+		spacing={2}
+		>
+			<Filters 
+        dataInicio={dataInicio} 
+        setDataInicio={setDataInicio} 
+        dataFim={dataFim} 
+        setDataFim={setDataFim}
+        nomeOperador={nomeOperador}
+        setNomeOperador={setNomeOperador}
+        setButtonClickCount={setFiltroClickCount}
+        />
+		</Stack>
+
 		<Stack
 		direction="row"
 		justifyContent="center"
